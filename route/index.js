46c587d00e4b6818b4e568478060a211e8d46adc
EMIRATES\S460242 <vivek.chauhan@emirates.com>
2020-04-12 13:07:53 +0400

    Update the code - move validation function in util

--- route/index.js
+++ route/index.js
@@ -1,27 +1,10 @@
 const express = require("express");
 const getSearchResult = require("./../src");
 const router = express.Router();
-const Joi = require("@hapi/joi");
+const { schema } = require("../util/util");
 
 router.get("/gethotels", async (request, response, next) => {
   try {
-    const schema = Joi.object()
-      .keys({
-        city: Joi.string().empty(""),
-        hotelname: Joi.string().empty(""),
-        priceRange: Joi.string()
-          .empty("")
-          .regex(/^\$\d+[.]?\d{1,2}\:\$\d+[.]?\d{1,2}$/i)
-          .label("Please enter the price Range in  format"),
-        dateRange: Joi.string()
-          .empty("")
-          .regex(
-            /^((0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4})\:((0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4})/i
-          )
-          .label("Please enter the date Range in DD-MM-YYYY:DD-MM-YYYY format"),
-      })
-      .or("city", "hotelname", "priceRange", "dateRange");
-
     const data = {
       city: request.query.city || "",
       hotelname: request.query.hotelname || "",
--- util/util.js
+++ util/util.js
@@ -1,41 +1,59 @@
 const moment = require("moment");
+const Joi = require("@hapi/joi");
 
 module.exports = {
   serviceResponse: (status, data) => {
     return {
       status: status,
-      data: data
+      data: data,
     };
   },
-  getPrice: priceRange => {
+  getPrice: (priceRange) => {
     if (priceRange) {
       const priceFrom = priceRange.split(":")[0];
       const priceTo = priceRange.split(":")[1];
 
       return {
         priceFrom: parseFloat(priceFrom.replace(/\$|,/g, "")),
-        priceTo: parseFloat(priceTo.replace(/\$|,/g, ""))
+        priceTo: parseFloat(priceTo.replace(/\$|,/g, "")),
       };
     }
   },
-  getDates: dateRange => {
+  getDates: (dateRange) => {
     const dFrom = dateRange.split(":")[0];
     const dTo = dateRange.split(":")[1];
 
     return {
       dateFrom: moment(dFrom, "DD-MM-YYYY"),
-      dateTo: moment(dTo, "DD-MM-YYYY")
+      dateTo: moment(dTo, "DD-MM-YYYY"),
     };
   },
 
   filterArray: (array, filters) => {
     const filterKeys = Object.keys(filters);
-    return array.filter(item => {
+    return array.filter((item) => {
       // validates all filter criteria
-      return filterKeys.every(key => {
+      return filterKeys.every((key) => {
         if (typeof filters[key] !== "function") return true;
         return filters[key](item[key]);
       });
     });
-  }
+  },
+
+  schema: Joi.object()
+    .keys({
+      city: Joi.string().empty(""),
+      hotelname: Joi.string().empty(""),
+      priceRange: Joi.string()
+        .empty("")
+        .regex(/^\$\d+[.]?\d{1,2}\:\$\d+[.]?\d{1,2}$/i)
+        .label("Please enter the price Range in  format"),
+      dateRange: Joi.string()
+        .empty("")
+        .regex(
+          /^((0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4})\:((0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4})/i
+        )
+        .label("Please enter the date Range in DD-MM-YYYY:DD-MM-YYYY format"),
+    })
+    .or("city", "hotelname", "priceRange", "dateRange"),
 };
